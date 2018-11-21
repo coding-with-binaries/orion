558ce292edb0ca1f811a57905ca711a1da108383
Varun Sharma <coding.with.binaries@gmail.com>
2018-11-22 00:12:42 +0530

    Converting to typescript project

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root') as HTMLElement
+);
+registerServiceWorker();
