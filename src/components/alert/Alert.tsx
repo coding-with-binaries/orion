a347d74c9e3bae5165a7234bf83a1d901e527ad2
Varun Sharma <coding.with.binaries@gmail.com>
2018-12-15 04:35:54 +0530

    Adding dismissable alert component using react hooks

---
+++ src/components/alert/Alert.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+  IoIosCheckmarkCircle,
+  IoIosClose,
+  IoIosInformationCircle,
+  IoIosNuclear,
+  IoIosWarning
+} from 'react-icons/io';
+import './Alert.css';
+
+type Props = Partial<DefaultProps>;
+
+type DefaultProps = Readonly<typeof defaultProps>;
+
+const defaultProps = {
+  type: 'info' as 'info' | 'success' | 'warning' | 'error',
+  dismissable: true as boolean
+};
+
+export const Alert: React.SFC<Props> = props => {
+  const [dismissed, setDismissed] = React.useState(false);
+  const { type, children, dismissable } = props;
+
+  const mapIconWitType = () => {
+    switch (type) {
+      case 'success':
+        return <IoIosCheckmarkCircle size="24" />;
+      case 'warning':
+        return <IoIosWarning size="24" />;
+      case 'error':
+        return <IoIosNuclear size="24" />;
+      default:
+        return <IoIosInformationCircle size="24" />;
+    }
+  };
+
+  const dismissAlert = () => setDismissed(true);
+
+  return (
+    <>
+      {!dismissed ? (
+        <div className={`orion-alert ${type}`}>
+          <span className={`icon-wrapper ${type}`}>{mapIconWitType()}</span>
+          <span className="children">{children}</span>
+          <span className="dismiss">
+            {dismissable ? (
+              <IoIosClose cursor="pointer" size="24" onClick={dismissAlert} />
+            ) : null}
+          </span>
+        </div>
+      ) : null}
+    </>
+  );
+};
+
+Alert.defaultProps = defaultProps;
